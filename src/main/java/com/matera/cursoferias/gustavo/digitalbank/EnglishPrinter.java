82e07bc5ec6d9760b7e31e0e39d650afc02c890b
gustavoborges25 <gustavoborges25@gmail.com>
2020-01-23 19:33:18 -0300

    corrigindo import, removendo profile, removendo arquivo errado

--- src/main/java/com/matera/cursoferias/gustavo/digitalbank/EnglishPrinter.java
+++ src/main/java/com/matera/cursoferias/gustavo/digitalbank/EnglishPrinter.java
@@ -1,10 +1,8 @@
 package com.matera.cursoferias.gustavo.digitalbank;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("en")
 public class EnglishPrinter implements Printer {
 
 	@Override
--- src/main/java/com/matera/cursoferias/gustavo/digitalbank/PortuguesePrinter.java
+++ src/main/java/com/matera/cursoferias/gustavo/digitalbank/PortuguesePrinter.java
@@ -1,6 +1,5 @@
 package com.matera.cursoferias.gustavo.digitalbank;
 
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
